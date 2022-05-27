ba08cc4b7a52b660617b77c1d11d5f4e30f788c8
danielgdiaz <danielgdiaz92@gmail.com>
2022-05-27 13:21:26 +0200

    test can't delete product in cart

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -38,7 +38,15 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     patch product_url(@product), params: { product: {  title: @title. title, description: @product.description, image_url: @product.image_url, price: @product.price } }
     assert_redirected_to product_url(@product)
   end
+  
+  test "can't delete product in cart" do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
+    end
 
+    assert_redirected_to products_url
+  end
+  
   test "should destroy product" do
     assert_difference('Product.count', -1) do
       delete product_url(@product)
@@ -46,4 +54,5 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to products_url
   end
+
 end
