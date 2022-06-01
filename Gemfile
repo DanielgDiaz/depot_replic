f3dd1a5961ec6aae4f7b0ac4ef6e32bea18f32f4
danielgdiaz <danielgdiaz92@gmail.com>
2022-06-01 11:31:06 +0200

    15- iteration J1, J2 & tests

--- Gemfile
+++ Gemfile
@@ -20,7 +20,8 @@ gem 'jbuilder', '~> 2.7'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
+
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
@@ -53,3 +54,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 #START:as_xml
 #END:as_xml
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
-#---
-# Excerpted from "Agile Web Development with Rails 6",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material,
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose.
-# Visit http://www.pragmaticprogrammer.com/titles/rails6 for more book information.
-#---
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy 
+  end
+
+  resources :users
   resources :orders
   resources :line_items
   resources :carts
