cb4e6f6ca04833b8667b222237d01f678f072957
danielgdiaz <danielgdiaz92@gmail.com>
2022-06-02 11:34:45 +0200

    17- iteration L3

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,3 @@
-#---
-# Excerpted from "Agile Web Development with Rails 6",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material,
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose.
-# Visit http://www.pragmaticprogrammer.com/titles/rails6 for more book information.
-#---
 Rails.application.routes.draw do
   get 'admin' => 'admin#index'
   controller :sessions do
@@ -24,6 +16,8 @@ Rails.application.routes.draw do
   resources :products do
     get :who_bought, on: :member
   end
+
+  
 
   scope '(:locale)' do
     resources :orders
